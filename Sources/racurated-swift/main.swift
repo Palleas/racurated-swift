a6ce9bec6fb25651b91e6be253a28d7d22db06d1
Romain Pouclet <romain.pouclet@gmail.com>
2017-11-29 10:32:24 -0800

    Add default model

--- Sources/racurated-swift/main.swift
+++ Sources/racurated-swift/main.swift
@@ -1,1 +1,25 @@
-print("Hello, world!")
+struct Version {
+    let rawValue: String
+}
+
+struct Project {
+    enum Kind {
+        case tool
+        case framework
+        case app
+    }
+
+    enum Platform {
+        case iOS
+        case macOS
+        case other
+    }
+
+    let name: String
+    let description: String
+    let version: Version
+    let highlights: Set<String>
+    let repository: URL
+    let platform: Platform
+    let kind: Kind
+}
